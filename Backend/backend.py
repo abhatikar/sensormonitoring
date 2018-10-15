fb307205a457c52775751f366930ad7a4d485fb6
Abhijeet Bhatikar <Abhijeet4jobs@gmail.com>
2018-10-15 13:51:09 +0100

    Changes
    Signed-off-by: Abhijeet Bhatikar <Abhijeet4jobs@gmail.com>

--- Backend/backend.py
+++ Backend/backend.py
@@ -7,6 +7,8 @@ import requests
 import json
 import ast
 from elasticsearch import Elasticsearch
+import dateutil.parser as dp
+
  
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
@@ -17,47 +19,22 @@ class SensorData(Resource):
 	def get(self, start_idx):
 		res=es.search(index='sensordata',body={ "sort" : [ { "@timestamp" : {"order" : "asc"} } ],'size' : 1000, 'from' : start_idx, 'query':{ 'match_all':{} }}) 
 		return res['hits']['hits']
-    
-	def post(self):
-		return {'status':'success'}
 
-class Tracks(Resource):
-	def get(self):
-		res=es.search(index='sensordata',body={
-        'query':{
-            'bool':{
-                'must':{
-                    'match':{
-                        'deviceId':'SBS01'
-                    }
-                },
-                "filter":{
-                    "range":{
-                        "deviceValue":{
-                            "gt":25
-                        }
-                    }
-                }
-            }
-        }
-    })
-
-		return {'hello': 'world'}
-    
-class Employees_Name(Resource):
-	def get(self, id_1):
-		return {'hello3': id_1}
-#    def get(self, employee_id):
-#        conn = db_connect.connect()
-#        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-#        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-#        return jsonify(result)
-
+class SensorDataDate(Resource):
+	def get(self, start_date, end_date):
+		print(start_date);
+		print(end_date);
+		sdate=dp.parse(start_date);
+		edate=dp.parse(end_date);
+		stimestamp=sdate.strftime('%s');
+		etimestamp=edate.strftime('%s');
+		print(int(stimestamp)*1000);
+		print(int(etimestamp)*1000);
+		res=es.search(index='sensordata',body={ "sort" : [ { "@timestamp" : {"order" : "asc"} } ],'size' : 1000, 'from' : 0, 'query':{ 'range':{ "@timestamp" : { "gte": int(stimestamp)*1000, "lte": int(etimestamp)*1000 }} }})
+		return res['hits']['hits']
 
 api.add_resource(SensorData, '/sensordata/<start_idx>') # Route_1
-api.add_resource(Tracks, '/tracks') # Route_2
-api.add_resource(Employees_Name, '/employees/<id_1>') # Route_3
-
+api.add_resource(SensorDataDate, '/sensordata/<start_date>/<end_date>') # Route_2
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=5000)
