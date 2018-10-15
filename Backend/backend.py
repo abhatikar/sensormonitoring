4beafa91c71d495f82ae612f0ca289613b51d096
Abhijeet Bhatikar <Abhijeet4jobs@gmail.com>
2018-10-15 17:22:54 +0100

    looping logic
    Signed-off-by: Abhijeet Bhatikar <Abhijeet4jobs@gmail.com>

--- Backend/backend.py
+++ Backend/backend.py
@@ -22,6 +22,7 @@ class SensorData(Resource):
 
 class SensorDataDate(Resource):
 	def get(self, start_date, end_date):
+		final_obj=[]
 		print(start_date);
 		print(end_date);
 		sdate=dp.parse(start_date);
@@ -31,7 +32,18 @@ class SensorDataDate(Resource):
 		print(int(stimestamp)*1000);
 		print(int(etimestamp)*1000);
 		res=es.search(index='sensordata',body={ "sort" : [ { "@timestamp" : {"order" : "asc"} } ],'size' : 1000, 'from' : 0, 'query':{ 'range':{ "@timestamp" : { "gte": int(stimestamp)*1000, "lte": int(etimestamp)*1000 }} }})
-		return res['hits']['hits']
+		print(res['hits']['total'])
+		nr_req = int(res['hits']['total']) / 1000;
+		modulo = int(res['hits']['total']) % 1000;
+		if modulo > 0:
+			nr_req=nr_req+1;
+		print(int(nr_req));
+		for counter in range(0, int(nr_req)):
+			print(counter*1000)
+			res=es.search(index='sensordata',body={ "sort" : [ { "@timestamp" : {"order" : "asc"} } ],'size' : 1000, 'from' : 1000*counter, 'query':{ 'range':{ "@timestamp" : { "gte": int(stimestamp)*1000, "lte": int(etimestamp)*1000 }} }})
+			final_obj += res['hits']['hits'];
+		print(len(final_obj))
+		return final_obj
 
 api.add_resource(SensorData, '/sensordata/<start_idx>') # Route_1
 api.add_resource(SensorDataDate, '/sensordata/<start_date>/<end_date>') # Route_2
