826df5f3e55f9784c6d0f098a23c36107bc1c0ae
Abhijeet Bhatikar <abhijeet4jobs@gmail.com>
2018-10-22 22:16:54 +0100

    Remove the TLS feature

--- Firmware/esp32-coldchain-fw/RPI-Gateway/ble_sensorclient.js
+++ Firmware/esp32-coldchain-fw/RPI-Gateway/ble_sensorclient.js
@@ -1,26 +1,8 @@
 var noble = require('noble');
 const mqtt = require ('mqtt');
-var fs = require('fs');
 
-var KEY = fs.readFileSync('certs/client.key');
-var CERT = fs.readFileSync('certs/client.crt');
-var CAfile = [fs.readFileSync('certs/all-ca.crt')];
+var client = mqtt.connect('mqtt://localhost');
 
-var options = {
-	host: 'mqttserver',
-	port: 8883,
-	protocol: 'mqtts',
-	protocolId: 'MQIsdp',
-	ca: CAfile,
-	key: KEY,
-	cert: CERT,
-	secureProtocol: 'TLSv1_2_method',
-	protocolId: 'MQIsdp',
-	protocolVersion: 3,
-	rejectUnauthorized: false
-};
-
-var client = mqtt.connect(options);
 client.on('connect', function() { // When connected
     console.log('connected');
 });
@@ -28,7 +10,7 @@ client.on('connect', function() { // When connected
 const peripheralAdress = "30:ae:a4:20:14:aa";
 //If name filtering is required
 const peripheralName = "SeeCat03";
-const char* sensorDataTopic = "deviceID/SeeCat03/sensorData";
+const sensorDataTopic = "deviceID/SeeCat03/sensorData";
 
 noble.on('stateChange', function (state) {
   console.log('on -> stateChange: ' + state);
@@ -54,28 +36,28 @@ noble.on('discover', function(peripheral) {
 	{
 	console.log(peripheral.address);
 	console.log(peripheral.localName);
-  peripheral.connect(function(error) {
-    console.log('connected to peripheral: ' + peripheral.uuid);
-    peripheral.discoverServices(['6e400001b5a3f393e0a9e50e24dcca9e'], function(error, services) {
-    //peripheral.discoverServices(['180f'], function(error, services) {
-      var batteryService = services[0];
-      console.log('discovered Temperature and Humidity service');
-      batteryService.discoverCharacteristics(['6e400003b5a3f393e0a9e50e24dcca9e'], function(error, characteristics) {
-      //batteryService.discoverCharacteristics(['2a19'], function(error, characteristics) {
-        var batteryLevelCharacteristic = characteristics[0];
+  	peripheral.connect(function(error) {
+    	console.log('connected to peripheral: ' + peripheral.uuid);
+    	peripheral.discoverServices(['6e400001b5a3f393e0a9e50e24dcca9e'], function(error, services) {
+    	//peripheral.discoverServices(['180f'], function(error, services) {
+      	var thSensorService = services[0];
+      	console.log('discovered Temperature and Humidity service');
+      	thSensorService.discoverCharacteristics(['6e400003b5a3f393e0a9e50e24dcca9e'], function(error, characteristics) {
+      	//thSensorService.discoverCharacteristics(['2a19'], function(error, characteristics) {
+        var thSensorValueCharacteristic = characteristics[0];
         console.log('discovered sensor value characteristic');
-        batteryLevelCharacteristic.on('data', function(data, isNotification) {
+        thSensorValueCharacteristic.on('data', function(data, isNotification) {
 		console.log(data.toString('utf8'));
 		client.publish(sensorDataTopic, data.toString('utf8'));
         });
         // to enable notify
-        batteryLevelCharacteristic.subscribe(function(error) {
+        thSensorValueCharacteristic.subscribe(function(error) {
           console.log('sensor value notification on');
         });
       });
     });
   });
- peripheral.once('disconnect', function() {
+ 	peripheral.once('disconnect', function() {
           console.log('Peripheral has been disconnected');
 	  process.exit(0);
          });
