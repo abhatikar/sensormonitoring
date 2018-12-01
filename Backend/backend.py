488a610f6e91d5fbf6ab630a3f1beab74f8be8dc
Abhijeet Bhatikar <abhijeet4jobs@gmail.com>
2018-12-01 23:22:08 +0000

    Code cleanup and TODO list

--- Backend/backend.py
+++ Backend/backend.py
@@ -17,34 +17,26 @@ api = Api(app)
 
 class SensorData(Resource):
 	def get(self, start_idx):
-		res=es.search(index='sensordata',body={ "sort" : [ { "@timestamp" : {"order" : "asc"} } ],'size' : 1000, 'from' : start_idx, 'query':{ 'match_all':{} }}) 
+		res=es.search(index='sensordata',body={ 'sort' : [ { '@timestamp' : {'order' : 'asc'} } ],'size' : 1000, 'from' : start_idx, 'query':{ 'match_all':{} }})
 		return res['hits']['hits']
 
 class SensorDataDate(Resource):
 	def get(self, start_date, end_date):
 		final_obj=[]
-		"""print(start_date);
-		print(end_date);
-		sdate=dp.parse(start_date);
-		edate=dp.parse(end_date);
-		stimestamp=sdate.strftime('%s');
-		etimestamp=edate.strftime('%s');
-		print(int(stimestamp)*1000);
-		print(int(etimestamp)*1000);"""
-		res = es.search(index='sensordata', doc_type='readings', scroll='2m', body={ "sort" : [ { "@timestamp" : {"order" : "asc"} } ],'size' : 1000, 'query':{ 'range':{ "@timestamp" : { "gte": int(start_date)*1000, "lte": int(end_date)*1000 }} }})
+		res = es.search(index='sensordata', doc_type='readings', scroll='2m', body={ 'sort' : [ { '@timestamp' : {'order' : 'asc'} } ],'size' : 1000, 'query':{ 'range':{ '@timestamp' : { 'gte': int(start_date)*1000, 'lte': int(end_date)*1000 }} }})
 		sid = res['_scroll_id'];
 		scroll_size = res['hits']['total']
 		print(res['hits']['total'])
 		final_obj += res['hits']['hits'];
 		# Start scrolling
 		while (scroll_size > 0):
-			print ("Scrolling...")
+			print ('Scrolling...')
 			res = es.scroll(scroll_id = sid, scroll = '2m')
 			# Update the scroll ID
 			sid = res['_scroll_id']
 			# Get the number of results that we returned in the last scroll
 			scroll_size = len(res['hits']['hits'])
-			print("scroll size: " + str(scroll_size))
+			print('scroll size: ' + str(scroll_size))
 			final_obj += res['hits']['hits'];
 			# Do something with the obtained page
 		print(len(final_obj))
--- Subscriber/esp32_subscriber.py
+++ Subscriber/esp32_subscriber.py
@@ -9,14 +9,14 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print("Connected to broker")
+        print('Connected to broker')
         global Connected                #Use global variable
         Connected = True                #Signal connection
     else:
-        print("Connection failed")
+        print('Connection failed')
 
 def on_message(client, userdata, message):
-	print("Message received:" + message.payload.decode('utf8'));
+	print('Message received:' + message.payload.decode('utf8'));
 	data=json.loads(message.payload.decode('utf8'));
 	dbdata = {}
 	dbdata['deviceValue'] = data['temperature'];
@@ -29,35 +29,35 @@ def on_message(client, userdata, message):
 	dbdata['deviceParameter'] = 'Humidity';
 	tt=es.index(index='sensordata', doc_type='readings', body=dbdata);
 	print(dbdata);
-	client.publish("sensorData/update", 'data updated' ,0)
+	client.publish('sensorData/update', 'data updated' ,0)
 	#print(tt);
 
 def on_subscribe(client, userdata, mid, granted_qos):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+    print('Subscribed: ' + str(mid) + ' ' + str(granted_qos))
 
 def on_log(mqttc, userdata, level, string):
     print(string)
 
-client = mqtt.Client("seecat_broker")
+client = mqtt.Client('seecat_broker')
 client.on_message = on_message
 client.on_connect = on_connect
 client.on_subscribe = on_subscribe
-# Uncomment to enable debug messages
+#Uncomment to enable debug messages
 #client.on_log = on_log
 
 Connected = False   #global variable for the state of the connection
 
-client.connect("iot.eclipse.org", 1883, 60)
+client.connect('iot.eclipse.org', 1883, 60)
 
 #while Connected != True:    #Wait for connection
 #    time.sleep(0.1)
 
-client.subscribe("deviceID/+/sensorData", 0);
+client.subscribe('deviceID/+/sensorData', 0);
 
 try:
 	client.loop_forever()
 
 except KeyboardInterrupt:
-    print("exiting")
+    print('exiting')
     client.disconnect()
     client.loop_stop()
