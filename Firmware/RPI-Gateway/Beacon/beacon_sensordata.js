fba46c801c710bbaeb116ea5592ad0af8002beb8
Abhijeet Bhatikar <abhijeet4jobs@gmail.com>
2018-12-03 21:46:44 +0000

    Minor changes

--- Firmware/RPI-Gateway/Beacon/beacon_sensordata.js
+++ Firmware/RPI-Gateway/Beacon/beacon_sensordata.js
@@ -1,17 +1,17 @@
 var noble = require('noble');
 const mqtt = require ('mqtt');
 
-var options = {host:'iot.eclipse.org', port:1883};
+var options = {host:'192.168.43.129', port:1883};
 var client = mqtt.connect(options);
 
 client.on('connect', function() { // When connected
     console.log('connected');
 });
 
-const peripheralAddress = "XX:XX:XX:XX:XX:XX"; //esp32 address
+const peripheralAddress = "30:ae:a4:1c:01:c2"; //esp32 address
 //If name filtering is required
-const peripheralName = "SeeCat03";
-const sensorDataTopic = "deviceID/SeeCat03/sensorData";
+const peripheralName = "SeeCat07";
+const sensorDataTopic = "deviceID/SeeCat07/sensorData";
 
 noble.on('stateChange', function(state) {
   if (state === 'poweredOn') {
